451971ac7093a5e0f0e3df72ca7a11993db6747d
JuliaKinash <117223088+JuliaKinash@users.noreply.github.com>
2022-11-21 21:05:24 -0200

    Task 4

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,5 +1,12 @@
+from Bio.Phylo._cdao_owl import element
+from fontTools.subset.svg import xpath
 from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webdriver import WebDriver
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from webdriver_manager.core import driver
+
+from utils.settings import DEFAULT_LOCATOR_TYPE
 
 
 class BasePage():
@@ -16,3 +23,11 @@ class BasePage():
     def get_page_title(self, url):
         self.driver.get(url)
         return self.driver.title
+
+        element - driver.find_element(by=By.XPATH, value=xpath)
+        element_text = element.text
+        assert expected_text == element_text
+
+    def wait_for_element_to_be_clicable(self, locator, locator_type=DEFAULT_LOCATOR_TYPE):
+        wait = WebDriverWait(self.driver, 10)
+        wait.until(EC.element_to_be_clickable((locator_type, locator)))
--- pages/dashboard.py
+++ pages/dashboard.py
@@ -1,15 +1,24 @@
 import time
 
 from pages.base_page import BasePage
-
-
 class Dashboard(BasePage):
-        expected_title = "Scouts panel"
-        dashboard_url ='https://scouts-test.futbolkolektyw.pl/'
-        add_player_button_xpath = "//main/div[3]/div[2]/div/div/a/button/span[1]"
-        def title_of_page(self):
-            time.sleep(4)
-            assert self.get_page_title(self.dashboard_url) == self.expected_title
+    button_xpath = "//*[@id='login']"
+    scouts_panel_button_xpath = "//h6[contains(@class,'MuiTypography-h6')]"
+    main_page_button_xpath = "//ul[1]/div[1]/div[2]/span"
+    players_count = "//main/div[2]/div[1]//b"
+    events_count_xpath = "//main/div[2]/div[4]/div/div[1]"
+    dev_team_photo_xpath = "//*[contains(@class,'MuiCardMedia-root jss8')]"
+    dev_team_button_xpath = '//main/div[3]//a[1]/span[2]'
+    matches_count_xpath = "//main/div [2]/div [2]//b"
+    activity_button_xpath = "//h2[contains(text(),'Activity')]"
+    expected_title = "Scouts panel"
+    dashboard_url ='https://scouts-test.futbolkolektyw.pl/'
+    add_player_button_xpath = "//main/div[3]/div[2]/div/div/a/button/span[1]"
+    futbal_kolektyw_logo_xpath ="//*[@title='Logo Scouts Panel']"
+    wait = WebDriverWait(driver, 10)
+    def title_of_page(self):
+        self.wait_for_element_to_be_clicable(self.futbal_kolektyw_logo_xpath)
+        assert self.get_page_title(self.dashboard_url) == self.expected_title
 
         def click_on_the_add_player_button(self):
             self.click_on_the_element(self.add_player_button_xpath)
